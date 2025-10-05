f21bf7051d63eb7b4b1835176aec556b703bac64
Bianca <biancaa.barbosa@outlook.com>
2025-10-04 22:39:10 -0300

    feat: Added stream route - exoplanet

--- src/controllers/stream/stream.controller.js
+++ src/controllers/stream/stream.controller.js
@@ -1,24 +1,42 @@
-import { streamServiceChunk } from '../../services/stream/stream.service.js';
+import { exoplanetClassifier } from "../../services/stream/stream.service.js";
+export class StreamController {
+  static async exoplanetClassifier(req, res) {
+    const { mode, dataset, data } = req.body;
 
-const streamController = (req, res) => {
+    if (!mode && !dataset && !data) {
+      return res.status(400).json({ error: "Missing required fields" });
+    }
 
-  const body = req.body;
+    if(dataset === "k2" && (!data.sy_pnum || !data.soltype || !data.pl_orbper)) {
+      return res.status(400).json({ error: "Missing mandatory data" });
+    }
 
-  streamServiceChunk(
-    body.endpoint || 'health'
-  )
-    .then((response) => {
-      if (response) {
-        res.status(200).send({ message: 'Streaming started', data: response });
-      } else {
-        res.status(404).send({ message: 'No data received' });
-      }
-    })
-    .catch((err) => {
-      res.status(500).send({ message: 'Error', error: err });
-    });
-};
+    if(dataset === "keppler" && (!data.koi_fpflag_ss || !data.koi_fpflag_nt || !data.koi_fpflag_co || !data.koi_fpflag_ec || !data.koi_model_snr || !data.koi_prad)) {
+      return res.status(400).json({ error: "Missing mandatory data" });
+    }
 
-export {
-  streamController
-};
+    // validate tess mandatory data
+
+    res.setHeader("Content-Type", "text/event-stream");
+    res.setHeader("Cache-Control", "no-cache");
+    res.setHeader("Connection", "keep-alive");
+
+    try {
+      await exoplanetClassifier(
+        "predict_manual",
+        dataset,
+        data,
+        (chunk) => {
+          res.write(`data: ${chunk}\n\n`);
+        }
+      );
+
+      res.write("event: done\ndata: end of stream\n\n");
+      res.end();
+    } catch (err) {
+      res.write(`event: error\ndata: ${err.message}\n\n`);
+      res.end();
+    }
+  }
+
+}
